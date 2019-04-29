3eac0fbee8016ceb9cfdc6007238dc2b2b1836c3
Linus Styrén <linus122xbb@gmail.com>
2019-04-29 02:52:05 +0200

    first commit

---
+++ Assets/Scripts/DialogHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogHandler : MonoBehaviour
+{
+
+    public string[] dialog;
+    public string[] actions;
+    public AudioClip[] narration;
+
+    public SceneHandler sceneHandler;
+
+    public TextMeshProUGUI script;
+    public Button next;
+    public Button[] buttons;
+
+    private int dialogIndex = 0;
+
+    public void Initialize(int dialogIndex)
+    {
+        this.dialogIndex = dialogIndex;
+        next.gameObject.SetActive(true);
+        buttons[0].transform.gameObject.SetActive(false);
+        buttons[1].transform.gameObject.SetActive(false);
+        buttons[2].transform.gameObject.SetActive(false);
+        buttons[3].transform.gameObject.SetActive(false);
+
+        NextDialog();
+    }
+
+    public void NextDialog()
+    {
+        if (dialogIndex == dialog.Length && actions.Length == 0)
+        {
+            sceneHandler.PreviousScene(4);
+            return;
+        }
+
+        if (dialogIndex < narration.Length) sceneHandler.PlayNarration(narration[dialogIndex]);
+        script.text = dialog[dialogIndex++];        
+
+        if (dialogIndex == dialog.Length)
+        {            
+            if (actions.Length == 0) return;
+
+            next.gameObject.SetActive(false);
+            for (int i = 0; i < actions.Length; i++)
+            {
+                buttons[i].transform.gameObject.SetActive(true);
+                buttons[i].GetComponentInChildren<TextMeshProUGUI>().text = actions[i];
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneHandler : MonoBehaviour
+{
+
+    [Serializable]
+    public struct NamedScene
+    {
+        public string action;
+        public DialogHandler scene;
+    }
+    public NamedScene[] scenes;
+
+    public AudioSource audioPlayer;
+
+    private Dictionary<string, DialogHandler> scenesDict;
+
+    private DialogHandler currentScene;
+    private DialogHandler previousScene;
+
+    private void Start()
+    {
+        scenesDict = new Dictionary<string, DialogHandler>();
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            scenesDict[scenes[i].action] = scenes[i].scene;
+        }
+
+        NextScene("start");
+    }
+
+    public void PlayNarration(AudioClip line)
+    {
+        audioPlayer.Stop();
+        audioPlayer.clip = line;
+        audioPlayer.Play();
+    }
+
+    public void NextScene(string action)
+    {
+        if (currentScene)
+        {
+            previousScene = currentScene;
+            currentScene.gameObject.SetActive(false);
+        }
+        currentScene = scenesDict[action];
+        currentScene.Initialize(0);
+        currentScene.gameObject.SetActive(true);
+    }
+
+    public void PreviousScene(int dialogIndex)
+    {
+        currentScene.gameObject.SetActive(false);
+        currentScene = previousScene;
+        currentScene.Initialize(dialogIndex);
+        currentScene.gameObject.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit(0);
+    }
+}
